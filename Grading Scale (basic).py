1f8979d3913792c60d732ddaff94b81ce3076b7c
MoreClarity <60890481+MoreClarity@users.noreply.github.com>
2022-08-17 14:22:23 -0400

    Add files via upload
    This is a basic flow of code for a grading scale using "IF" & "Else" statements

---
+++ Grading Scale (basic).py
@@ -0,0 +1,29 @@
+
+# Ryan Hodgson Grading scale
+
+score = float(input("Enter score: "))
+
+#evaluate score
+
+if score >= 90:
+
+    print("A")
+
+else:
+    if score >= 80:
+
+        print("B")
+    else:
+
+        if score >= 70:
+
+            print("C")
+
+        else:
+
+            if score >=  60:
+
+                print("D")
+            else:
+
+                print("f")
